94b4667d9ee2a299d997a9b97cad82ce70fe7388
jinzza456 <jinzza456@gmail.com>
2021-10-12 12:15:08 +0900

    multipl without 3*2

--- multi.go
+++ multi.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func main() {
 	for dan := 1; dan <= 9; dan++ {
+
 		fmt.Printf("%d단\n", dan)
 
 		for j := 1; j <= 9; j++ {
+			if dan == 3 && j == 2 {
+				continue
+			}
+
 			fmt.Printf("%d * %d = %d\n", dan, j, dan*j)
 		}
 		fmt.Println()
